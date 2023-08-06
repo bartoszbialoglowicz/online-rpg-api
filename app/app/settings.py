2060a16bcc0471601ca229b5de95a66a01d56bf0
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-08-06 19:00:06 +0200

    feat: installed Pillow

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,11 +30,13 @@ ALLOWED_HOSTS = [
     '192.168.0.143',
     '192.168.0.66',
     '127.0.0.1',
+    '192.168.100.15'
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://192.168.0.66:3000'
+    'http://192.168.0.66:3000',
+    'http://192.168.100.15:3000'
 ]
 # Application definition
 
@@ -131,6 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
